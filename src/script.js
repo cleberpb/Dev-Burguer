67195a860f4375ce5e513de2a174d9962ecdb9be
cleberpb <36317652+cleberpb@users.noreply.github.com>
2024-05-02 14:21:08 -0300

    Finalizando projeto

--- src/script.js
+++ src/script.js
@@ -129,16 +129,77 @@ function removeItemCart(name) {
 // Pegar o que foi digitado no input
 addressInput.addEventListener("input", function (event) {
     let inputValue = event.target.value;
+
+    if (inputValue !== "") {
     addressInput.classList.remove("border-red-500");
     addressWarn.classList.add("hidden");
-  
+    }
 })
 
+
+// Finalizar pedido
 checkoutBtn.addEventListener("click", function () {
+    const isOpen = checkRestaurantOpen();
+
+    if (!isOpen) {
+        Toastify ({
+            text: "Ops! Restaurante fechado no momento!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "#ef4444",
+            },
+        }).showToast();
+
+        return;
+    }
+
+
     if (cart.length === 0) return;
     if (addressInput.value === "") {
         addressWarn.classList.remove("hidden");
         addressInput.classList.add("border-red-500");
-
+        return;
     }
+
+    // Enviar o pedido para api do whatsapp
+    const cartItems = cart.map((item) => {
+        return (
+         ` ${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price} |`
+        )
+    }).join("");
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "+5583999968405"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank");
+
+    // Limpar o carrinho
+    cart = [];
+    updateCartModal();
+    
 })
+
+
+
+// Verificar a hora de funcionamento do card horário
+function checkRestaurantOpen() {
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+
+}
+
+const spanItem = document.getElementById("date-span");
+const isOpen = checkRestaurantOpen();
+
+if (isOpen) {
+    spanItem.classList.remove("bg-red-500");
+    spanItem.classList.add("bg-green-600");
+} else {
+    spanItem.classList.remove("bg-green-600");
+    spanItem.classList.add("bg-red-500");
+}
